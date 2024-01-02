12cbdedfdabe8e88ab3f8fcf2863a245ec77c9b2
Priya <priaysu@gmail.com>
2024-01-02 12:06:54 -0600

    creating a expense project using git repository

--- frontend.sh
+++ frontend.sh
@@ -3,19 +3,17 @@ dnf install nginx -y
 
 #coping the conf file
 cp expense.conf  /etc/nginx/default.d/expense.conf
-# Enabling and starting the nginx
-systemctl enable nginx
-systemctl start nginx
 
 #removing the files /usr/share/nginx/html/
 rm -rf /usr/share/nginx/html/*
 
-#downloading the contets if frontend zip
+#downloading the contents if frontend.zip
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 
 #changing the directory and unzip the content
-cd /usr/share/nginx/html/
+cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-#Editing the expense file
-vim /etc/nginx/default.d/expense.conf
+# Enabling and starting the ngin
+systemctl enable nginx
+systemctl restart nginx
